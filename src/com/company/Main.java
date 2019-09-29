1170454a33c3862af0ceba9e909c338b21a3efab
Liudmyla Studentova <mila.studentova@gmail.com>
2019-09-29 20:08:58 +0300

    Commit3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World2");
+                    String str1 = args[0];
+            int marks = Integer.parseInt(str1);
+            switch (marks) {
+                case 90:
+                    System.out.println("You marks:"+marks+" and rank is A");
+                    break;
+                case 80:
+                    System.out.println("You marks:"+marks+" and rank is B");
+                    break;
+                case 70:
+                    System.out.println("You marks:"+marks+" and rank is C");
+                    break;
+                default:
+                    System.out.println("You marks:"+marks+" and rank is FAIL");
 
 	// write your code here
     }
-}
+}}
